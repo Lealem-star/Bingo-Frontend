ff10f03afaefebe4a60e697eca292af63ae2ff18
Lealem-1 <lealem.meseret@aastustudent.edu.et>
2025-10-03 13:46:59 +0300

    env update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'dieretic-semilunate-ouida.ngrok-free.dev'
+    ]
+  },
   define: {
     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://bingo-back-2evw.onrender.com'),
     'import.meta.env.VITE_WS_URL': JSON.stringify(process.env.VITE_WS_URL || 'wss://bingo-back-2evw.onrender.com/ws'),
